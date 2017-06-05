36f2e0af22c3b7c95cdd7bfe8d45366c105b0d54
Rafael Neves <rafaeljneves@gmail.com>
2017-06-04 21:09:03 -0300

    Refatora geral

--- eventex/settings.py
+++ eventex/settings.py
@@ -29,6 +29,9 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
 
+DEFAULT_FROM_EMAIL = 'contato@eventex.com.br'
+
+
 # Application definition
 
 INSTALLED_APPS = (
